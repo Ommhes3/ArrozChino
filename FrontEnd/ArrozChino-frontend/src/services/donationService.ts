72676db66820b6af6672acc4817c91cc66d86789
Ommhes Leon <ommhessamuek@gmail.com>
2026-05-27 09:35:30 -0500

    Cambios de la main

--- FrontEnd/ArrozChino-frontend/src/services/donationService.ts
+++ FrontEnd/ArrozChino-frontend/src/services/donationService.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL ?? "http://192.168.130.33:8000";
 
 export type Donation = {
   donation_id: string;
